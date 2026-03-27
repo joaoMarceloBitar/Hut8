8cf1b88e5c81767f6c8cbc71310df14bbda338d6
DevTheusP <matheusp.estudos@gmail.com>
2026-03-27 12:58:51 -0300

    feat: estrutura inical navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './paginas/Home/Home'
+import Navbar from './components/layout/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/projetos' element={<></>}/>
-            <Route path='/equipe' element={<></>}/>
+          <Route path='/' element={<Home />} />
+          <Route path='/projetos' element={<></>} />
+          <Route path='/equipe' element={<></>} />
         </Routes>
       </BrowserRouter>
-   </>
+    </>
   )
 }
 
---
+++ src/components/layout/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+function Navbar() {
+    return (
+        <nav>
+            <div>
+                <img src="/Logo-Hut8.svg" alt="Logo Hut8" />
+            </div>
+            <div>
+                <a href="#">Home</a>
+                <a href="#">Projetos</a>
+                <a href="#">Serviços</a>
+                <a href="#">Nossa Equipe</a>
+                <a href="#">Fale Conosco</a>
+            </div>
+
+        </nav>
+    )
+}
+
+export default Navbar
+
+
