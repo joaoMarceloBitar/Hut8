0cfba3afe63250755e53c105d782717edb5b03d6
DevTheusP <matheusp.estudos@gmail.com>
2026-03-27 19:43:04 -0300

    feat: navbar concluida

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -1,17 +1,22 @@
+import styles from './Navbar.module.css'
+
 function Navbar() {
     return (
-        <nav>
-            <div>
-                <img src="/Logo-Hut8.svg" alt="Logo Hut8" />
+        <nav className={styles.navbar}>
+            <div className={styles.container}>
+                <div className={styles.logo}>
+                    <img src="/Logo-Hut8.svg" alt="Logo Hut8" />
+                </div>
+                <div className={styles.links}>
+                    <a className={styles.link} href="#">Home</a>
+                    <a className={styles.link} href="#">Projetos</a>
+                    <a className={styles.link} href="#">Serviços</a>
+                    <a className={styles.link} href="#">Nossa Equipe</a>
+                    <div>
+                        <a className={styles.botao} href="#">Fale Conosco</a>
+                    </div>
+                </div>
             </div>
-            <div>
-                <a href="#">Home</a>
-                <a href="#">Projetos</a>
-                <a href="#">Serviços</a>
-                <a href="#">Nossa Equipe</a>
-                <a href="#">Fale Conosco</a>
-            </div>
-
         </nav>
     )
 }
