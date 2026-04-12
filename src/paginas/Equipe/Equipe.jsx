73af22fcedbe3145e2d0b0c856ddabe37fab547d
joaoMarceloBitar <joaomarcelodu@gmail.com>
2026-04-12 16:51:51 -0300

    globals.css implementada, para padronizacao da fonte

--- src/paginas/Equipe/Equipe.jsx
+++ src/paginas/Equipe/Equipe.jsx
@@ -1,5 +1,23 @@
-function Equipe(){
+import styles from './Equipe.module.css'
+function Equipe() {
 
+    return (
+        <section>
+            <div className={styles.container}>
+                <div className={styles.texto}>
+                    <div className={styles.tag}>
+                        <div className={styles.bola}></div>
+                        <p>A empresa júnior da computação</p>
+                    </div>
+                    <h1>
+                        Conheça nossa Equipe
+                    </h1>
+                    <p>Nosso time de jovens universitários apaixonados por tecnologia, empreendedorismo e inovação</p>
+                </div>
+                <img src="foto1-equipe.svg" alt="" className={styles.imagem_1} />
+            </div>
+        </section>
+    )
 
 }
 
